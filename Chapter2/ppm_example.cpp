1e2233fe862f86bb805afb9380f9dad1d292b1e6
Jason Fiammetta <jason.fiammetta@gmail.com>
2024-04-19 16:55:29 -0400

    Progress tracker

--- Chapter2/ppm_example.cpp
+++ Chapter2/ppm_example.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 
 int main() {
@@ -8,7 +7,12 @@ int main() {
 
     std::cout << "P3\n" << width << ' ' << height << "\n255\n";
 
+    int threshold = 9;
+
     for (int j = 0; j < height; j++) {
+
+        std::clog << "\rLine " << j << " of " << height << std::flush;
+
         for (int i = 0; i < width; i++) {
             int r = i;
             int g = j;
@@ -17,4 +21,7 @@ int main() {
             std::cout << r << ' ' << g << ' ' << b << '\n';
         }
     }
+    std::clog << "\rdone                     \n"; 
+    // extra spaces overwrite last log line
+    // without them it writes: done 255 of 256
 }
